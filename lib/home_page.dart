86511af49ab972104180cd2e7983da05fea6acda
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2021-09-30 22:58:27 -0500

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,17 +13,67 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de Cultivos"),
+        backgroundColor: Colors.green,
       ),
       body: Consumer<DetallesProvider>(
         builder: (context, prodsProv, child) {
-          return ListView.builder(
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return Text("Pending");
-            },
-          );
+          return GridView.count(
+
+              // Gridview con 3 cuadros
+              crossAxisCount: 3,
+              children: List.generate(30, (index) {
+                // 30 -> (List.count / 3)
+                // Caracteristicas de las tarjetas del Grid
+                return Card(
+                  elevation: 10.0,
+                  margin: EdgeInsets.all(8.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: Align(
+                      alignment: Alignment.center,
+
+                      // Contenido de las tarjetas
+                      child: Column(
+                        children: <Widget>[
+                          // Imagen de cultivo
+                          Padding(
+                            padding: EdgeInsets.all(5.0),
+                            child: Image.network(
+                              "https://cdn.shopify.com/s/files/1/0326/7189/t/65/assets/pf-e820b2e0--mother-tree-forest.jpg?v=1619557558", // link -> prodsProv.image
+                              height: 100.0,
+                              width: 100.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+
+                          SizedBox(
+                            height: 5.0,
+                          ),
+
+                          // Texto con nombre de cultivo
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                            child: Text(
+                              "$index", //"$index" -> prodsProv.name
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                );
+              }));
         },
       ),
     );
   }
 }
+
+// ListView.builder(
+//             itemCount: 10,
+//             itemBuilder: (BuildContext context, int index) {
+//               return Text("Pending");
+//             },
+//           );
