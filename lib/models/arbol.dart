dbf403fc129858cf4bcabd0a4a65d30c92cc4e7a
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2021-11-05 09:37:39 -0600

    Update arbol.dart

--- lib/models/arbol.dart
+++ lib/models/arbol.dart
@@ -1,65 +1,33 @@
 import 'package:equatable/equatable.dart';
 
-class Arbol extends Equatable {
-  final String? tipo;
-  final String? imgUrl;
-  final int? id;
-  final String? name;
-  final String? estado;
-  final String? abono;
-  final String? descripcion;
+import 'detalles.dart';
 
-  const Arbol(
-      {this.tipo,
-      this.imgUrl,
-      this.id,
-      this.name,
-      this.estado,
-      this.abono,
-      this.descripcion});
+class Messages extends Equatable {
+  final List<Detalles>? detalles;
 
-  factory Arbol.fromJson(Map<String, dynamic> json) => Arbol(
-        tipo: json['tipo'] as String?,
-        imgUrl: json['imgUrl'] as String?,
-        id: json['id'] as int?,
-        name: json['name'] as String?,
-        estado: json['estado'] as String?,
-        abono: json['abono'] as String?,
-        descripcion: json['descripcion'] as String?,
+  const Messages({this.detalles});
+
+  factory Messages.fromJson(Map<String, dynamic> json) => Messages(
+        detalles: (json['detalles'] as List<dynamic>?)
+            ?.map((e) => Detalles.fromJson(e as Map<String, dynamic>))
+            .toList(),
       );
 
   Map<String, dynamic> toJson() => {
-        'tipo': tipo,
-        'imgUrl': imgUrl,
-        'id': id,
-        'name': name,
-        'estado': estado,
-        'abono': abono,
-        'descripcion': descripcion,
+        'detalles': detalles?.map((e) => e.toJson()).toList(),
       };
 
-  Arbol copyWith({
-    String? tipo,
-    String? imgUrl,
-    int? id,
-    String? name,
-    String? estado,
-    String? abono,
-    String? descripcion,
+  Messages copyWith({
+    List<Detalles>? detalles,
   }) {
-    return Arbol(
-      tipo: tipo ?? this.tipo,
-      imgUrl: imgUrl ?? this.imgUrl,
-      id: id ?? this.id,
-      name: name ?? this.name,
-      estado: estado ?? this.estado,
-      abono: abono ?? this.abono,
-      descripcion: descripcion ?? this.descripcion,
+    return Messages(
+      detalles: detalles ?? this.detalles,
     );
   }
 
+  /*@override
+	bool get stringify => true;*/
+
   @override
-  // TODO: implement props
-  List<Object?> get props =>
-      [tipo, imgUrl, id, name, estado, abono, descripcion];
+  List<Object?> get props => [detalles];
 }
