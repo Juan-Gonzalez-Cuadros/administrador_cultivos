c34b2d5e15e3e71a34041dd2a949039767ae5112
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2021-11-24 21:41:10 -0600

    Update detalles.dart

--- lib/detalles.dart
+++ lib/detalles.dart
@@ -1,16 +1,11 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Detalles extends StatefulWidget {
-  Detalles({Key? key}) : super(key: key);
+class Detalles extends StatelessWidget {
+  final data;
+  Detalles(this.data);
 
-  @override
-  _DetallesState createState() => _DetallesState();
-}
-
-class _DetallesState extends State<Detalles> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,46 +13,41 @@ class _DetallesState extends State<Detalles> {
         title: Text("Detalles de Cultivo"),
         backgroundColor: Colors.green,
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            //<Widget>
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40),
-                  ),
-                  //image: DecorationImage(image: AssetImage("prodsProv.image")),
-                ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 15.0,
               ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.white,
-                //child: ListTile{
-                child: Column(
-                  //title:
-                  children: [
-                    //<Widget>
-                    Text(
-                      "Planta",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+              Container(
+                child: Image.network(data['img']),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Text(
+                    data['id'],
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w400,
                     ),
-                  ],
+                  ),
                 ),
-                //},
               ),
-            ),
-          ],
-        ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  data['tipo'],
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.normal),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
