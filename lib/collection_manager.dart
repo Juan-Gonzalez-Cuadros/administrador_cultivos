4a6f6b0ddbf45f808cb481c14be71b3406cd94b5
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2021-11-25 00:34:47 -0600

    Update collection_manager.dart

--- lib/collection_manager.dart
+++ lib/collection_manager.dart
@@ -6,12 +6,18 @@ class CollectioManager {
   CollectioManager(this._arboles);
 
   Future<void> agregarArbol({required String tipo}) async {
+    QuerySnapshot productCollection =
+        await FirebaseFirestore.instance.collection('arboles').get();
+    int productCount = productCollection.size;
     await _arboles.add({
       'tipo': tipo,
       'estado': true,
       'fecha': DateTime.now(),
       'abono': '',
-      'plaga': ''
+      'plaga': '',
+      'id': productCount,
+      'img':
+          'https://cdn.shopify.com/s/files/1/0326/7189/t/65/assets/pf-e820b2e0--mother-tree-forest.jpg?v=1619557558'
     });
   }
 }
