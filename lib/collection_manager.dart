4c8a7534eef69cdbfc00bb90a92d3bf18856df52
Juan-Gonzalez-Cuadros <55216941+Juan-Gonzalez-Cuadros@users.noreply.github.com>
2021-11-25 21:32:57 -0600

    Update collection_manager.dart

--- lib/collection_manager.dart
+++ lib/collection_manager.dart
@@ -9,7 +9,11 @@ class CollectioManager {
     QuerySnapshot productCollection =
         await FirebaseFirestore.instance.collection('arboles').get();
     int productCount = productCollection.size;
-    await _arboles.add({
+
+    DocumentReference<Map<String, dynamic>> arboles = FirebaseFirestore.instance
+        .collection('/arboles')
+        .doc(productCount.toString());
+    var myJSONObj = {
       'tipo': tipo,
       'estado': true,
       'fecha': DateTime.now(),
@@ -18,6 +22,35 @@ class CollectioManager {
       'id': productCount,
       'img':
           'https://cdn.shopify.com/s/files/1/0326/7189/t/65/assets/pf-e820b2e0--mother-tree-forest.jpg?v=1619557558'
-    });
+    };
+    arboles
+        .set(myJSONObj)
+        .then((value) => print("Document with CustomID added"))
+        .catchError((error) => print("Failed to add document: $error"));
+  }
+
+  Future<void> actualizarArbol(
+      {required String id,
+      required String estado,
+      required String abono,
+      required String plaga,
+      required String img,
+      required String tipo}) async {
+    bool b = estado.toLowerCase() == 'true';
+    DocumentReference<Map<String, dynamic>> arboles =
+        FirebaseFirestore.instance.collection('/arboles').doc(id);
+    var myJSONObj = {
+      'tipo': tipo,
+      'estado': b,
+      'fecha': DateTime.now(),
+      'abono': abono,
+      'plaga': plaga,
+      'id': id,
+      'img': img
+    };
+    arboles
+        .set(myJSONObj)
+        .then((value) => print("Document updated"))
+        .catchError((error) => print("Failed to update document: $error"));
   }
 }
